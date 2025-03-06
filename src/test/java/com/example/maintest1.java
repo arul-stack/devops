2d02004e6c9a8f4166750d97e41779295dad246a
arul-stack <23124011@srcas.ac.in>
2025-03-06 14:34:41 +0530

    Update maintest1.java

--- src/test/java/com/example/maintest1.java
+++ src/test/java/com/example/maintest1.java
@@ -1,16 +1,20 @@
 package com.example;
-import static org.junit.Asset.assetEquals;
+
+import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
-public class maintest1
-	{
-		private main1 main;
-	}
-public void setUp()
-	{
-		main=new main1();	
-	}
-public void testGreet()
-	{
-		assertEquals("Hello ,Jenkins !",main.greet());
-	}
+
+public class maintest1 {
+
+    private main1 main;
+
+    @Before
+    public void setUp() {
+        main = new main1();    
+    }
+
+    @Test
+    public void testGreet() {
+        assertEquals("Hello ,Jenkins !", main.greet());
+    }
+}
